d9f825eae01ec015da3102a7e982a8427cbe1f48
SteveBacross <ogoudisteve@gmail.com>
2025-03-05 15:33:31 +0100

    :sparkle: (Chat):Ajout du composant chat d'affichage des discussions et d'envoi de messages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Chat from './components/Chat';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Chat />
   </React.StrictMode>,
   document.getElementById('root')
 );
