76fb6f99e4c6eda3bbaa483961f1114b9a0a8d56
SteveBacross <ogoudisteve@gmail.com>
2025-03-07 13:59:54 +0100

    UI globa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+// import "./index.css";
+import "./global.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
